3bdb10e80f176220fb461695170112f16677a4bf
Nzelu Ozioma <nzeluozioma555@gmail.com>
2023-10-23 11:46:49 +0100

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ title }) => {
   return (
     <header className="Header">
       <h1>{title}</h1>
-      <img className="pic" src="/icons.png" />
+      <img className="pic" src={process.env.PUBLIC_URL + "/icons.png"} />
     </header>
   );
 };
